be30e5deb8ef39b750469c06e5bb7beb508d2f86
stlck <johs.duvig@gmail.com>
2017-01-22 22:49:08 +0100

    meshing it

---
+++ Assets/Scripts/Environment/test/CATest.cs
@@ -0,0 +1,242 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ProceduralToolkit;using System.Linq;
+
+public class CATest : MonoBehaviour
+{
+    public float GeneratePercentage = 60;
+    public int Size = 15;
+    public int neighborsMin = 3;
+
+    public bool ShowGizmos = false;
+
+    public List<List<Vector3>> Regions;
+    public List<Vector3> Region;
+    int[,,] map;
+    Mesh output;
+
+    // Use this for initialization
+    void Start()
+    {
+        map = new int[Size, Size, Size];
+        output = new Mesh();
+    }
+
+    void OnGUI()
+    {
+        if(GUILayout.Button("DO CA"))
+            Generate();
+        
+        if (GUILayout.Button("SMOOTH CA"))
+            Smooth();
+
+        if(GUILayout.Button("DO MESH"))
+            CreateAsMesh();
+
+        if(GUILayout.Button("DO REGIONS"))
+            doregions();
+
+        if (GUILayout.Button("Mesh REgion"))
+            regionsToMesh();
+
+        if (GUILayout.Button("carve"))
+            carve();
+
+        if(GUILayout.Button("Do ALl"))
+        {
+
+            Generate();
+            Smooth();
+            Smooth();
+            Smooth();
+            doregions();
+            regionsToMesh();
+            carve();
+        }
+
+        ShowGizmos = GUILayout.Toggle(ShowGizmos, "Show gizmos");
+    }
+
+    void carve()
+    {
+        var y = (int)Region.Average(m => m.y);
+        var x = (int)Region.Average(m => m.x);
+        var z = (int)Region.Average(m => m.z);
+        var c = new GameObject();
+        MeshDraft interior = new MeshDraft();
+        for (int i = x; i > Region.Min(m => m.x) -4; i--)
+        {
+            //if(!inBounds(i+2, y,z) || map[i + 2, y, z] > 0)
+            { 
+                var e = MeshDraft.Hexahedron(1, 1, 1);
+                e.Move(Vector3.up * y + Vector3.forward * z + Vector3.right * i);
+                interior.Add(e);
+            }
+            //interior.Add(MeshDraft.Quad(Vector3.up * y + Vector3.forward * z + Vector3.right * i, Vector3.one, Vector3.one));
+        }
+        output = interior.ToMesh();
+        c.transform.SetParent(transform);
+        c.transform.localPosition = Vector3.zero;
+        c.AddComponent<MeshFilter>().mesh = output;
+        c.AddComponent<MeshRenderer>().material = GetComponent<MeshRenderer>().material;
+    }
+
+    void doregions()
+    {
+        int[,,] all = map.Clone() as int[,,];
+        Regions = new List<List<Vector3>>();
+
+        //while(all.Length > 0)
+        for (int i = 0; i < Size; i++)
+            for (int j = 0; j < Size; j++)
+                for (int k = 0; k < Size; k++)
+                {
+                    if (all[i, j, k] > 0)
+                    {
+                        var r = new List<Vector3>();
+                        singleregion(r ,i, j, k);
+                        Regions.Add(r);
+                    }
+                }
+        map = all;
+    }
+
+    void singleregion(List<Vector3> region, int x, int y, int z)
+    {
+        //region.Add()
+        //region[i, j, k] = 1;
+        region.Add(new Vector3(x, y, z));
+        map[x, y, z] = 0;
+        for (int i = x - 1; i <= x + 1; i++)
+            for (int j = y - 1; j <= y + 1; j++)
+                for (int k = z - 1; k <= z + 1; k++)
+                    if (inBounds(i,j,k) && map[i, j, k] > 0)
+                        singleregion(region, i, j, k);                  
+    }
+
+    void regionsToMesh()
+    {
+        MeshDraft draft = new MeshDraft();
+        Region = Regions.OrderBy(m => m.Count).Last();
+
+        foreach (var p in Region.Where(r => neightborCount6(r) < 6))
+        {
+            {
+                var e = MeshDraft.Hexahedron(1, 1, 1);
+                e.Move(p);
+                draft.Add(e);
+            }
+        }
+
+        output = draft.ToMesh();
+        GetComponent<MeshFilter>().mesh = output;
+    }
+
+    void CreateAsMesh()
+    {
+        MeshDraft draft = new MeshDraft();
+        for (int i = 0; i < Size; i++)
+            for (int j = 0; j < Size; j++)
+                for (int k = 0; k < Size; k++)
+                {
+                    if(map[i,j,k] > 0)
+                    {
+                        var e = MeshDraft.Hexahedron(1, 1, 1);
+                        e.Move(indexToPosition(i,j,k));
+                        draft.Add(e);
+                    }
+                }
+
+        output = draft.ToMesh();
+        GetComponent<MeshFilter>().mesh = output;
+    }
+
+    Vector3 indexToPosition(int i, int j, int k)
+    {
+        return Vector3.right * i + Vector3.up * j + Vector3.forward * k;
+    }
+
+    void Generate()
+    {
+        for (int i = 0; i < Size; i++)
+            for (int j = 0; j < Size; j++)
+                for (int k = 0; k < Size; k++)
+                {
+                    map[i, j, k] = Random.Range(0, 100) > GeneratePercentage ? 1 : 0;
+                }
+    }
+
+    void Smooth()
+    {
+        for (int i = 0; i < Size; i++)
+            for (int j = 0; j < Size; j++)
+                for (int k = 0; k < Size; k++)
+                {
+                    var nCount = neighborCount(i, j, k);
+                    if (nCount > neighborsMin)
+                        map[i, j, k] = 1;
+                    else if (nCount < neighborsMin)
+                        map[i, j, k] = 0;
+                }
+    }
+
+    int neightborCount6(Vector3 pos)
+    {
+        int x = (int)pos.x;
+        int y = (int)pos.y;
+        int z = (int)pos.z;
+        /*
+        var ret = 0;
+        for (int i = x - 1; i <= x + 1; i++)
+            for (int j = y - 1; j <= y + 1; j++)
+                for (int k = z - 1; k <= z + 1; k++)
+                {
+                    if (inBounds(i, j, k))//i >= 0 && i < Size && j >= 0 && j < Size && k >= 0 && k < Size)
+                        ret += map[i, j, k];
+                }
+        */
+        var ret = 0;
+        ret += inBounds(x-1, y, z) ? map[x-1, y, z] : 0;
+        ret += inBounds(x+1, y, z) ? map[x+1, y, z] : 0;
+        ret += inBounds(x, y-1, z) ? map[x, y-1, z] : 0;
+        ret += inBounds(x, y+1, z) ? map[x, y+1, z] : 0;
+        ret += inBounds(x, y, z-1) ? map[x, y, z-1] : 0;
+        ret += inBounds(x, y, z+1) ? map[x, y, z+1] : 0;
+
+        return ret;
+    }
+
+    int neighborCount(int x, int y, int z)
+    {
+        var ret = 0;
+        for (int i = x-1; i <= x+1; i++)
+            for (int j = y-1; j <= y+1; j++)
+                for (int k = z-1; k <= z+1; k++)
+                {
+                    if (inBounds(i,j,k))//i >= 0 && i < Size && j >= 0 && j < Size && k >= 0 && k < Size)
+                        ret += map[i, j, k];
+                }
+
+        return ret -1;
+    }
+
+    bool inBounds(int i, int j, int k)
+    {
+        return i >= 0 && i < Size && j >= 0 && j < Size && k >= 0 && k < Size;
+    }
+
+    public void OnDrawGizmos()
+    {
+        if(map != null && ShowGizmos)
+        {
+            for(int i = 0; i < Size;i++)
+                for(int j = 0; j < Size;j++)
+                    for(int k = 0; k < Size;k++)
+                    {
+                        if(map[i,j,k] > 0)
+                            Gizmos.DrawCube(Vector3.right * i + Vector3.up* j + Vector3.forward * k, Vector3.one);
+                    }
+        }
+    }
+}
---
+++ Assets/Scripts/Environment/test/CreateRoomsTest.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class CreateRoomsTest : MonoBehaviour {
+
+    public List<Room> Rooms = new List<Room>();
+    public List<Corridor> Corridors = new List<Corridor>();
+
+    public int RoomMinSize = 4;
+    public int RoomMAxSize = 12;
+    public int RoomCount = 5;
+    public int RoomCountVariation = 2;
+    public Vector3 Size;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("DO ROOMS"))
+            CreateRooms();
+        if (GUILayout.Button("Connect Rooms"))
+            ConnectRooms();
+    }
+
+    void ConnectRooms()
+    {
+        Rooms.Sort((a, b) => a.Position.y.CompareTo(b.Position.y));
+        //Rooms = Rooms.OrderBy((r) => r.Position.y).ThenBy((r) => )
+        Corridors = new List<Corridor>();
+
+        //foreach (var r in Rooms)
+        for(int i = 0; i < Rooms.Count -1; i++)
+        {
+            var b1 = new Bounds(Rooms[i].Position, Rooms[i].size);
+            var b2 = new Bounds(Rooms[i + 1].Position, Rooms[i + 1].size);
+
+            var p1 = b1.ClosestPoint(Rooms[i + 1].Position);
+            var p2 = b2.ClosestPoint(Rooms[i].Position);
+            Corridors.Add(new Corridor(p1, p2));
+        }
+    }
+
+    void CreateRooms()
+    {
+        var rooms = RoomCount + Random.Range(-RoomCountVariation, RoomCountVariation);
+        Rooms = new List<Room>();
+        for (int i = 0; i < rooms; i ++)
+        {
+            Rooms.Add(new Room(new Vector3(getIntRandom( Size.x), getIntRandom(Size.y), getIntRandom(Size.z)), Random.Range(RoomMinSize, RoomMAxSize), Random.Range(RoomMinSize, RoomMAxSize)));
+        }
+    }
+
+    int getIntRandom(float mod)
+    {
+        return Mathf.RoundToInt( Random.value * mod);
+    }
+
+    void OnDrawGizmos()
+    {
+        if(Rooms != null)
+            Rooms.ForEach((r) => Gizmos.DrawCube(r.Position, r.size + Vector3.up * .5f));
+        if (Corridors != null)
+            Corridors.ForEach((r) => Gizmos.DrawLine(r.Start, r.End));
+    }
+    
+
+    [System.Serializable]
+    public class Room
+    {
+        public Vector3 Position;
+        public Vector3 size;
+        public int Width;
+        public int Height;
+
+        public Room(Vector3 p, int w, int h)
+        {
+            Position = p;
+            Width = w;
+            Height = h;
+            size = Vector3.forward * Height + Vector3.right * Width;
+        }
+    }
+
+    public class Corridor
+    {
+        public Vector3 Start;
+        public Vector3 End;
+
+        public Corridor(Vector3 s, Vector3 e)
+        {
+            Start = s;
+            End = e;
+        }
+    }
+}
