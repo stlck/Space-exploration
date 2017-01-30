7f2247eba0d51914c0bb616260a2e30f2ee053a4
Johs <johs.duvig@gmail.com>
2017-01-30 16:12:30 +0100

    new station mesh test

--- Assets/Scripts/Environment/test/CATest.cs
+++ Assets/Scripts/Environment/test/CATest.cs
@@ -5,21 +5,18 @@ using ProceduralToolkit;using System.Linq;
 
 public class CATest : MonoBehaviour
 {
-    public float GeneratePercentage = 60;
-    public int Size = 15;
-    public int neighborsMin = 3;
-
+    public int Size = 30;
+    public int CutIterations = 4;
     public bool ShowGizmos = false;
 
-    public List<List<Vector3>> Regions;
-    public List<Vector3> Region;
+    public List<Cell> Cells = new List<Cell>();
+    public List<Room> Rooms = new List<Room>();
     int[,,] map;
     Mesh output;
 
     // Use this for initialization
     void Start()
     {
-        map = new int[Size, Size, Size];
         output = new Mesh();
     }
 
@@ -27,190 +24,38 @@ public class CATest : MonoBehaviour
     {
         if(GUILayout.Button("DO CA"))
             Generate();
-        
-        if (GUILayout.Button("SMOOTH CA"))
-            Smooth();
-        if (GUILayout.Button("INCREASE SIZE"))
-            IncreaseMapSize(Size * 2);
+
+        if (GUILayout.Button("DO ROOMS"))
+            doRooms();
 
         if (GUILayout.Button("DO MESH"))
             CreateAsMesh();
-
-        if(GUILayout.Button("DO REGIONS"))
-            doregions();
-
-        //if (GUILayout.Button("Mesh REgion"))
-        //    regionsToMesh();
-
-        if (GUILayout.Button("carve"))
-            carve2();
-
-        if(GUILayout.Button("Do ALl"))
-        {
-            float real = Time.realtimeSinceStartup;
-            var spawner = new AsteroidSpawner();
-            Debug.Log("Region to mesh " + (Time.realtimeSinceStartup - real));
-            spawner.Generate(Size);
-        Debug.Log("1 " + (Time.realtimeSinceStartup - real));
-            spawner.Smooth();
-        Debug.Log("2 " + (Time.realtimeSinceStartup - real));
-            spawner.Smooth();
-        Debug.Log("3 " + (Time.realtimeSinceStartup - real));
-            spawner.IncreaseMapSize(20);
-        Debug.Log("4 " + (Time.realtimeSinceStartup - real));
-            spawner.Smooth();
-        Debug.Log("5 " + (Time.realtimeSinceStartup - real));
-            spawner.Smooth();
-        Debug.Log("6 " + (Time.realtimeSinceStartup - real));
-            spawner.IncreaseMapSize(26);
-        Debug.Log("7 " + (Time.realtimeSinceStartup - real));
-            spawner.Smooth();
-        Debug.Log("8 " + (Time.realtimeSinceStartup - real));
-            spawner.doregions();
-
-        Debug.Log("9 " + (Time.realtimeSinceStartup - real));
-            spawner.Carve(transform);
-        Debug.Log("10 " + (Time.realtimeSinceStartup - real));
-        }
-
+        
         ShowGizmos = GUILayout.Toggle(ShowGizmos, "Show gizmos");
     }
-    void carve2()
-    {
-        //var real = Time.realtimeSinceStartup;
-        var y = (int)Region.Average(m => m.y);
-        var x = (int)Region.Average(m => m.x);
-        var z = (int)Region.Average(m => m.z);
-        var lower = new GameObject();
-        var upper = new GameObject();
-        var p = Vector3.up * y + Vector3.forward * z + Vector3.right * x;
-        var xTo = Region.Min(m => m.x) - 4;
-        var lowerRegion = new List<Vector3>();
         
-        // remove all at y + 1 except walls
-        var toRemove = Region.Where(m => m.y == y +1 && neightborCount6(m) == 6).ToList();
-        foreach (var t in toRemove)
-            Region.Remove(t);
-        toRemove.Clear();
-
-        // add a landing bridge
-        var sub = Region.Where(m => m.y == y && m.z == z);
-        var pathOut = new List<Vector3>();
-        for (int i = x; i > xTo; i--)
-        {
-            p.x++;
-            if (!sub.Contains(p))
-                Region.Add(p);
-
-           pathOut.Add(p);     
-        }
-
-        sub = Region.Where(m => m.y <= y + 1);
-        foreach (var tile in sub)
-        {
-            if (tile.y > y - 3 && /*neightborCount6(tile, Region) < 6 &&*/ !pathOut.Contains(tile - Vector3.up))
-                lowerRegion.Add(tile);
-            toRemove.Add(tile);
-        }
-
-        foreach (var t in toRemove)
-            Region.Remove(t);
-        
-        lower.transform.SetParent(transform);
-        lower.transform.localPosition = Vector3.zero;
-        lower.AddComponent<MeshRenderer>().material = GetComponent<MeshRenderer>().material;
-        StartCoroutine(regionToMeshCoroutine(lowerRegion, lower.AddComponent<MeshFilter>()));
-
-        upper.transform.SetParent(transform);
-        upper.transform.localPosition = Vector3.zero;
-        upper.AddComponent<MeshRenderer>().material = GetComponent<MeshRenderer>().material;
-        StartCoroutine(regionToMeshCoroutine(Region, upper.AddComponent<MeshFilter>()));
-    }
-
-    void doregions()
-    {
-        int[,,] all = map.Clone() as int[,,];
-        Regions = new List<List<Vector3>>();
-
-        //while(all.Length > 0)
-        for (int i = 0; i < Size; i++)
-            for (int j = 0; j < Size; j++)
-                for (int k = 0; k < Size; k++)
-                {
-                    if (all[i, j, k] > 0)
-                    {
-                        var r = new List<Vector3>();
-                        singleregion(r ,i, j, k);
-                        Regions.Add(r);
-                    }
-                }
-        map = all;
-        Region = Regions.OrderBy(m => m.Count).Last();
-    }
-
-    void singleregion(List<Vector3> region, int x, int y, int z)
-    {
-        region.Add(new Vector3(x, y, z));
-        map[x, y, z] = 0;
-        for (int i = x - 1; i <= x + 1; i++)
-            for (int j = y - 1; j <= y + 1; j++)
-                for (int k = z - 1; k <= z + 1; k++)
-                    if (inBounds(i,j,k) && map[i, j, k] > 0)
-                        singleregion(region, i, j, k);                  
-    }
-
-    void regionsToMesh(List<Vector3> region, MeshFilter target)
-    {
-        MeshDraft draft = new MeshDraft();
-        
-        var sub = region.Where(r => neightborCount6(r, region) < 6);
-        var dir = Directions.All & ~Directions.Down;
-        var size = Vector3.one;
-        foreach (var p in sub )
-        {
-            var e = MeshDraft.Hexahedron(size,size,size, dir);
-            e.Move(p);
-            draft.Add(e);
-        }
-
-        target.mesh = draft.ToMesh();
-    }
-
-    IEnumerator regionToMeshCoroutine(List<Vector3> region, MeshFilter target)
-    {
-        float real = Time.realtimeSinceStartup;
-        MeshDraft draft = new MeshDraft();
-
-        var size = Vector3.one;
-        int blocks = 0;
-        foreach (var p in region)
-        {
-            var e = MeshDraft.Hexahedron(1,1,1);
-            e.Move(p);
-            draft.Add(e);
-            if(blocks++ % 5 == 0)
-                yield return new WaitForEndOfFrame();
-        }
-
-        //output = draft.ToMesh();
-        target.mesh = draft.ToMesh();
-        target.gameObject.name = "done";
-    }
-
     void CreateAsMesh()
     {
         MeshDraft draft = new MeshDraft();
-        for (int i = 0; i < Size; i++)
-            for (int j = 0; j < Size; j++)
-                for (int k = 0; k < Size; k++)
-                {
-                    if(map[i,j,k] > 0)
-                    {
-                        var e = MeshDraft.Hexahedron(1, 1, 1);
-                        e.Move(indexToPosition(i,j,k));
-                        draft.Add(e);
-                    }
-                }
+
+        //for (int i = 0; i < Size; i++)
+        //    for (int j = 0; j < Size; j++)
+        //        for (int k = 0; k < Size; k++)
+        //        {
+        //            if(map[i,j,k] > 0)
+        //            {
+        //                var e = MeshDraft.Hexahedron(1, 1, 1);
+        //                e.Move(indexToPosition(i,j,k));
+        //                draft.Add(e);
+        //            }
+        //        }
+        int counter = 0;
+        foreach (var r in Rooms)
+        { 
+            var e = MeshDraft.Hexahedron(r.w, r.h, 1);
+            e.Move(Vector3.forward * r.center.y + Vector3.right * r.center.x + counter++ * Vector3.up);
+            draft.Add(e);
+        }
 
         output = draft.ToMesh();
         GetComponent<MeshFilter>().mesh = output;
@@ -220,52 +65,51 @@ public class CATest : MonoBehaviour
     {
         return Vector3.right * i + Vector3.up * j + Vector3.forward * k;
     }
-
-    void IncreaseMapSize(int newsize)
-    {
-        var newMap = new int[newsize, newsize, newsize];
-        int dif = (newsize - Size)/2;
-
-        for (int i = 0; i < Size; i++)
-            for (int j = 0; j < Size; j++)
-                for (int k = 0; k < Size; k++)
-                {
-                    newMap[i + dif, j+ dif, k+ dif] = map[i, j, k];
-                }
-
-        map = newMap;
-        Size = newsize;
-    }
-
+    
     void Generate()
     {
-        Size = 15;
-        map = new int[Size, Size, Size];
-        for (int i = 0; i < Size; i++)
-            for (int j = 0; j < Size; j++)
-                for (int k = 0; k < Size; k++)
-                {
-                    map[i, j, k] = Random.Range(0, 100) > GeneratePercentage ? 1 : 0;
-                }
+        Rooms.Clear();
+        Cells.Clear();
+        int iterations = CutIterations;
+        var start = new Cell( 0, 0, Size, Size);
+        Cells.Add(start);
+        splitCell(start, iterations);
+    }
+    
+    void splitCell(Cell cell, int i)
+    {
+        i--;
+        Cell cell2;
+        if(Random.value > 0.5f)
+        {
+            var t = cell.w / 2;// Random.Range(2,4);
+            cell2 = new Cell(cell.x + t, cell.y, cell.w - t, cell.h);
+            cell.w -= t;
+        }
+        else
+        {
+            var t = cell.h / 2;// Random.Range(2, 4);
+            cell2 = new Cell(cell.x, cell.y + t, cell.w, cell.h - t);
+            cell.h -= t;
+        }
+        Cells.Add(cell2);
+
+        if(i > 0)
+        {
+            splitCell(cell, i);
+            splitCell(cell2, i);
+        }
     }
 
-    void Smooth()
+    void doRooms()
     {
-        int[,,] temp = new int[Size, Size, Size];
-        for (int i = 0; i < Size; i++)
-            for (int j = 0; j < Size; j++)
-                for (int k = 0; k < Size; k++)
-                {
-                    { 
-                    var nCount = neighborCount(i, j, k);
-                        if (nCount >= neighborsMin)
-                            temp[i, j, k] = 1;
-                        else if (nCount < neighborsMin)
-                            temp[i, j, k] = 0;
-                    }
-                }
-        map = temp;
+        foreach(var c in Cells)
+        {
+            var r = new Room(c.x, c.y, c.w, c.h);
+            Rooms.Add(r);
+        }
     }
+
     int neightborCount6(Vector3 pos, List<Vector3> region)
     {
         var ret = 0;
@@ -326,6 +170,83 @@ public class CATest : MonoBehaviour
                         if(map[i,j,k] > 0)
                             Gizmos.DrawCube(Vector3.right * i + Vector3.up* j + Vector3.forward * k, Vector3.one);
                     }
+        }
+        
+        if (Rooms != null && Rooms.Count > 0 && ShowGizmos)
+            foreach (var c in Rooms)
+            {
+                Gizmos.color = new Color(c.x / (float)Size, c.y /(float)Size,1f,1f);
+                Gizmos.DrawCube(Vector3.forward * c.center.y + Vector3.right * c.center.x, Vector3.forward * c.h + Vector3.right * c.w + Vector3.up);
+            }
+        else if(Cells != null && ShowGizmos)
+            foreach (var c in Cells)
+            {
+                Gizmos.color = new Color(c.x / (float)Size, c.y / (float)Size, 1f, 1f);
+                Gizmos.DrawCube(Vector3.forward * c.center.y + Vector3.right * c.center.x, Vector3.forward * c.h + Vector3.right * c.w + Vector3.up);
+                
+            }
+    }
+
+    [System.Serializable]
+    public class Room
+    {
+        public float x;
+        public float y;
+        public float w;
+        public float h;
+
+        public Room(float _x, float _y, float _w, float _h)
+        {
+            this.x = _x;
+            this.y = _y;
+            this.w = _w;
+            this.h = _h;
+        }
+
+        public Point center
+        {
+            get
+            {
+                return new Point(x + w / 2, y + h / 2);
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class Cell
+    {
+        public float x;
+        public float y;
+        public float w;
+        public float h;
+
+        public Point center
+        {
+            get
+            {
+                return new Point(x + w / 2, y + h / 2);
+            }
+        }
+
+        public Cell(float _x, float _y, float _w, float _h)
+        {
+            this.x = _x;
+            this.y = _y;
+            this.w = _w;
+            this.h = _h;
+        }
+    }
+
+    [System.Serializable]
+    public class Point
+    {
+        public float x;
+        public float y;
+
+        public Point(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
         }
     }
 }
